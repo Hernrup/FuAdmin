d06f7ab578e7b04ec87a089ad3e8aaf438522d18
Mikael Hernrup <mikael@hernrup.se>
2014-10-29 22:17:57 +0100

    Unittesting

---
+++ tests/TmpTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class TmpTest extends PHPUnit_Framework_TestCase{
+    public function setUp() {
+
+    }
+
+    public function testGetAllPosts() {
+        $this->assertEquals(5, 1);
+    }
+}
